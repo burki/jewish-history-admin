8b7c6873ee5a1598002e2136e638a7514642345b
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2015-05-02 19:12:12 +0200

    Add e-smart-zoom to img.php

--- htdocs/img.php
+++ htdocs/img.php
@@ -4,9 +4,9 @@
  *
  * show enlarged image
  *
- * (c) 2009 daniel.burckhardt@sur-gmbh.ch
+ * (c) 2009-2015 daniel.burckhardt@sur-gmbh.ch
  *
- * Version: 2009-03-30 dbu
+ * Version: 2015-05-02 dbu
  *
  * Changes:
  */
@@ -26,8 +26,6 @@ $STRIP_SLASHES = get_magic_quotes_gpc();  // set to 1 in php3 where this functio
 $url = $width = $height = $caption = '';
 foreach (array('url', 'width', 'height', 'caption') as $key) {
   $$key = array_key_exists($key, $_GET) ? $_GET[$key] : '';
-  if ($STRIP_SLASHES)
-    $$key = stripslashes($$key);
 }
 
 $url_large = !isset($_GET['large']) || $_GET['large']
@@ -38,17 +36,93 @@ $url_large = !isset($_GET['large']) || $_GET['large']
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de_DE" xml:lang="de_DE">
 <head>
-  <title><?php echo htmlspecialchars($SITE['pagetitle']) ?> / image</title>
-  <link rel="stylesheet" type="text/css" href="css/style.php" />
+  <title><?php echo htmlspecialchars($SITE['pagetitle'], ENT_COMPAT, 'utf-8') ?> / image</title>
+  <link rel="stylesheet" href="./css/styles_img.css" type="text/css" />
+  <script src="./script/jquery-1.11.0.min.js"></script>
+  <script src="./script/e-smart-zoom-jquery.min.js"></script>
   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<!--[if lt IE 9]>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<script>
+			$(document).ready(function() {
+              resizeDivs();
+
+				$('#imageFullScreen').smartZoom({'containerClass':'zoomableContainer'});
+
+				$('#topPositionMap,#leftPositionMap,#rightPositionMap,#bottomPositionMap').bind("click", moveButtonClickHandler);
+  				$('#zoomInButton,#zoomOutButton').bind("click", zoomButtonClickHandler);
+
+				function zoomButtonClickHandler(e){
+			    	var scaleToAdd = 0.8;
+					if(e.target.id == 'zoomOutButton')
+						scaleToAdd = -scaleToAdd;
+					$('#imageFullScreen').smartZoom('zoom', scaleToAdd);
+			    }
+
+			    function moveButtonClickHandler(e){
+			    	var pixelsToMoveOnX = 0;
+					var pixelsToMoveOnY = 0;
+
+					switch(e.target.id){
+						case "leftPositionMap":
+							pixelsToMoveOnX = 50;
+						break;
+						case "rightPositionMap":
+							pixelsToMoveOnX = -50;
+						break;
+						case "topPositionMap":
+							pixelsToMoveOnY = 50;
+						break;
+						case "bottomPositionMap":
+							pixelsToMoveOnY = -50;
+						break;
+					}
+					$('#imageFullScreen').smartZoom('pan', pixelsToMoveOnX, pixelsToMoveOnY);
+			    }
+
+                function resizeDivs() {
+                  var vpw = $(window).width();
+                  var vph = $(window).height();
+                  $('#pageContent').css({'width': vpw + 'px'});
+                  $('#imgContainer').css({'width': vpw + 'px',
+                                         'height': vph + 'px'});
+                }
+
+			});
+		</script>
 </head>
 
 <body>
-<div class="Popup" style="width: <?php echo $width ?>;">
-<a href="javascript:window.close();">
-<img src="<?php echo htmlspecialchars($url_large) ?>" width="<?php echo intval($width) ?>" height="<?php echo intval($height) ?>" alt="Click to close" border="0" /></a>
-<div class="Caption">
-<p><?php echo preg_replace('/\n/', "<br />", htmlspecialchars($caption, ENT_COMPAT, 'utf-8')) ?></p></div>
-</div>
+  <!--<div class="Caption">
+    <p><?php echo preg_replace('/\n/', "<br />", htmlspecialchars($caption, ENT_COMPAT, 'utf-8')) ?></p>
+  </div>-->
+  <div id="page">
+    <div id="pageContent">
+        <div id="imgContainer">
+            <img id="imageFullScreen" src="<?php echo htmlspecialchars($url_large) ?>"/>
+        </div>
+        <div id="positionButtonDiv">
+            <p>Zoom :
+                <span>
+                    <img id="zoomInButton" class="zoomButton" src="./media/zoom/zoomIn.png" title="zoom in" alt="zoom in" />
+                    <img id="zoomOutButton" class="zoomButton" src="./media/zoom/zoomOut.png" title="zoom out" alt="zoom out" />
+                </span>
+            </p>
+            <p>
+                <span class="positionButtonSpan">
+                    <map name="positionMap" class="positionMapClass">
+                        <area id="topPositionMap" shape="rect" coords="20,0,40,20" title="move up" alt="move up"/>
+                        <area id="leftPositionMap" shape="rect" coords="0,20,20,40" title="move left" alt="move left"/>
+                        <area id="rightPositionMap" shape="rect" coords="40,20,60,40" title="move right" alt="move right"/>
+                        <area id="bottomPositionMap" shape="rect" coords="20,40,40,60" title="move bottom" alt="move bottom"/>
+                    </map>
+                    <img src="./media/zoom/position.png" usemap="#positionMap" />
+                </span>
+            </p>
+        </div>
+    </div>
+  </div>
 </body>
+
 </html>
